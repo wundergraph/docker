4bbf4051760210c5ffa84cb6b908c5ca82f1796d
starptech <deusdustin@gmail.com>
2022-08-21 18:56:14 +0200

    trigger

--- .wundergraph/wundergraph.config.ts
+++ .wundergraph/wundergraph.config.ts
@@ -32,7 +32,7 @@ configureWunderGraphApplication({
 			process.env.NODE_ENV === 'production'
 				? [
 						// change this before deploying to production to the actual domain where you're deploying your app
-						'http://localhost:3001',
+						'http://localhost:3000',
 				  ]
 				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
 	},
